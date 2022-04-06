e5a1e55b22fbca3ba162bfb8bc95ffb93b6625b7
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-04-06 10:29:30 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(num1,num2){
+    return num1 + num2
+}
+
+function subtract(num1, num2){
+    return num1 - num2
+}
+
+function multiply(num1, num2){
+    return num1 * num2
+}
+
+function divide(num1,num2){
+    return num1 / num2
+}
+
+function increment(number){
+    let increm = ++number
+    return increm
+}
+
+function decrement(n){
+    let decre = --n
+    return decre
+}
+
+function makeInt(n){
+    let converted = parseInt(n,10)
+    return converted
+}
+
+function preserveDecimal(n){
+    let convo = parseFloat(n)
+    return convo
+}
